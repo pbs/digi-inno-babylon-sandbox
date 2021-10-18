b1d7bd6e401364bb8cdf109b2726a8a6fe11b2bf
Useff Chase <useffc@gmail.com>
2021-10-17 21:56:19 -0400

    detailed readme and comments for part 2

--- src/scene.ts
+++ src/scene.ts
@@ -12,22 +12,31 @@ import {
 import { TransformableCubeNode, TransformableSphereNode } from "./helpers/mesh";
 import { setupGroundMaterial } from "./helpers/material";
 
+
+// Import @babylonjs/inspector to have it in scope
 import "@babylonjs/inspector";
 
 export const createScene = async (
   engine: Engine,
   canvas: HTMLCanvasElement
 ): Promise<Scene> => {
+  // Create scene
   const scene = new Scene(engine);
+
+  // Create materials for use
   const sceneMaterials = {
     matteGray: new StandardMaterial("matte-grey", scene),
     woodPanel: setupGroundMaterial(scene),
   };
   scene.clearColor = new Color4(184, 237, 255, 0.95);
 
+
+  // Ambient light source
   const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
   light.intensity = 0.55;
 
+
+  // Create ground and assignment of ground material
   const ground = MeshBuilder.CreateGround(
     "ground",
     { width: 100, height: 100 },
@@ -35,6 +44,8 @@ export const createScene = async (
   );
   ground.material = sceneMaterials.woodPanel;
 
+
+  // Create camera and attach control to the browser's Canvas element
   const flatSpaceCamera = new ArcRotateCamera(
     "basic-camera",
     -1.16,
@@ -43,9 +54,10 @@ export const createScene = async (
     new Vector3(0, 0, 0),
     scene
   );
-
   flatSpaceCamera.attachControl(canvas);
 
+
+  // Create meshes and assign materials
   const redBox = new TransformableCubeNode("red-box", scene, Color3.Red(), 10);
   redBox.position = new Vector3(-20, 5, 10);
   const greenSphere = new TransformableSphereNode(
@@ -56,6 +68,8 @@ export const createScene = async (
   );
   greenSphere.position = new Vector3(5, 5, 10);
 
+
+  // Show scene's debugging layer
   scene.debugLayer.show();
   return scene;
 };
